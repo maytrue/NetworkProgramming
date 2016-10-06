52e85e813ac105cfc8c89c0475871c42e2ff244b
guowei <guowzou@gmail.com>
2016-10-06 22:11:30 +0800

    endian convertion

--- ch3_endian_conv/ch3_endian_conv.c
+++ ch3_endian_conv/ch3_endian_conv.c
@@ -7,9 +7,23 @@
 //
 
 #include <stdio.h>
+#include <arpa/inet.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+
+    uint16_t host_port = 0x1234;
+    uint16_t net_port;
+    uint32_t host_addr = 0x12345678;
+    uint32_t net_addr;
+    
+    net_port = htons(host_port);
+    net_addr = htonl(host_addr);
+    
+    printf("host order port: %#x \n", host_port);
+    printf("net order port: %#x \n", net_port);
+    
+    printf("host order address: %#x \n", host_addr);
+    printf("net order address: %#x \n", net_addr);
+    
     return 0;
 }
